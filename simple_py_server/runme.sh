7045cda3f80921e8f5b131e34aa2100e3bb0fbf6
Subhankar De <desubhankar94@outlook.com>
2021-08-05 18:14:40 +0530

    dos2unix

--- simple_py_server/runme.sh
+++ simple_py_server/runme.sh
@@ -6,4 +6,4 @@ echo "Build done ..."
 
 echo "Starting Container: simple_py_server on port 8080 ..."
 docker run -d -p 8080:8080 --name simple_py_server simple_py_server
-echo "Containe is up ..."
+echo "Container is up ..."
