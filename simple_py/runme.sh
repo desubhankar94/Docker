0388649bc22b3cb06fa5c918b126a33aad779481
Subhankar De <desubhankar94@outlook.com>
2021-08-05 17:19:56 +0530

    runme.sh

---
+++ simple_py/runme.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Starting build ..."
+docker build . -t simple_py_server
+echo "Build done ..."
+
+echo "Starting Container: simple_py_server on port 8080 ..."
+docker run -d -p 8080:8080 --name simple_py_server simple_py_server
+echo "Containe is up ..."
